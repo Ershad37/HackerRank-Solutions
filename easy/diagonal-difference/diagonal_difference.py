6acac314a1a7832e87f9ecc18b7fa1d8d830482e
Ershad <ershad.moh37@gmail.com>
2026-01-24 17:00:42 +0100

    Add the solution for diagonal_difference

---
+++ easy/diagonal-difference/diagonal_difference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    prime = second = 0
+    i = 0
+    j = len(arr) - 1
+    for line in arr:
+        prime += line[i]
+        second += line[j]
+        i += 1
+        j -= 1
+    
+    return abs(prime - second)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
